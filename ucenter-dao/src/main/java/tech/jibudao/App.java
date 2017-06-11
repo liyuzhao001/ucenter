3e06fe1e09988141b5565c48286215a13b6b8fe4
liyuzhao <liyuzhao001@163.com>
2017-06-11 23:51:32 +0800

    初始化 UCenter

---
+++ ucenter-dao/src/main/java/tech/jibudao/App.java
@@ -0,0 +1,16 @@
+package tech.jibudao;
+
+
+import tech.jibudao.ucenter.entity.UsersEntity;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        UsersEntity user = new UsersEntity();
+
+        System.out.println("Hello World!");
+    }
+}
---
+++ ucenter-entity/src/main/java/tech/jibudao/ucenter/entity/UsersEntity.java
@@ -0,0 +1,70 @@
+package tech.jibudao.ucenter.entity;
+
+import java.util.Date;
+
+/**
+ * 用户
+ * Created by Administrator on 2017/6/11.
+ */
+public class UsersEntity {
+
+    private long user_id;
+
+    private String user_name;
+
+    private String user_pwd;
+
+    private Date create_time;
+
+    private Date update_time;
+
+    private int user_state;
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_pwd() {
+        return user_pwd;
+    }
+
+    public void setUser_pwd(String user_pwd) {
+        this.user_pwd = user_pwd;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    public Date getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(Date update_time) {
+        this.update_time = update_time;
+    }
+
+    public int getUser_state() {
+        return user_state;
+    }
+
+    public void setUser_state(int user_state) {
+        this.user_state = user_state;
+    }
+}
